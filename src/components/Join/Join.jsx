f469be941203c45beec9e562660322766ead249f
Valeriia <alggv.b.2828@gmail.com>
2025-07-25 19:27:24 +0200

    feat: Complete Join's right side with contact form for gym sign-ups

--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
-import './Join.css'
+import React, { useState } from 'react'; 
+import './Join.css';
+
 const Join = () => {
+  const [message, setMessage] = useState('');
+  const [messageType, setMessageType] = useState('');
+
+  const handleFormSubmit = () => {
+    setMessage('Your message has been sent successfully!');
+    setMessageType('success');
+  };
+
   return (
     <div className="Join" id="join-us">
       <div className="left-j">
@@ -10,13 +19,22 @@ const Join = () => {
           <span> LEVEL UP</span>
         </div>
         <div>
-          <span>YOUR  BODY</span>
+          <span>YOUR BODY</span>
           <span className='stroke-text'> WITH US</span>
         </div>
       </div>
-      <div className="right-j"></div>
+      <div className="right-j">
+        <form
+          className="email-container"
+          action="https://formspree.io/f/manbobyp" 
+          method="POST"
+        >
+          <input type="email" name='user_email' placeholder='Enter your email address' />
+          <button type="submit" className='btn btn-j'>Join Now</button> {/* type="submit" важливий */}
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Join
+export default Join;
